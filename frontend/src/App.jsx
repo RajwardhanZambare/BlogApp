b542434cf70bd71469d114722a252517bc2ae3e3
RajwardhanZambare <codesonlycode@gmail.com>
2026-07-04 19:57:23 +0530

    installed react-router-dom and created initial routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Create from './pages/Create.jsx'
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/login' element={<Login />}/>
+                <Route path='/register' element={<Register />}/>
+                <Route path='/create' element={<Create />}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
