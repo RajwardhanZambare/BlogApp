7f75b5523b9e6e6d245a9960cffe2bc1ce1694ba
RajwardhanZambare <codesonlycode@gmail.com>
2026-07-13 20:06:14 +0530

    modified routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import blogRoutes from './routes/blog.routes.js'
+import authRoutes from './routes/auth.routes.js'
+import { registerUser } from './controllers/auth.controller.js'
 
 const app = express()
 
 app.use(express.json()) //middleware
+
+app.use('/', blogRoutes)
+app.use('/api/auth', authRoutes)
 
 export default app
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,10 @@
 import express from "express"
+import { registerUser } from "../controllers/auth.controller.js"
 
 const router = express.Router()
 
 router.post('/register', (req, res) => {
-    return res.json({
-        message: "this is '/register' api"
-    })
+    registerUser(req, res)
 })
 
 export default router
