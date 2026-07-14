452d7efc8045b6bdbb1a463719b6d993291a318c
RajwardhanZambare <codesonlycode@gmail.com>
2026-07-14 17:51:57 +0530

    hide db password inside .env

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2'
 
 const host = process.env.DB_HOST
+const pass = process.env.DB_PASSWORD
 
 const db = mysql.createConnection({
     host:`${host}`,
     user:"root",
-    password:"rajmysql2006",
+    password: `${pass}`,
     database:"BlogApp"
 })
 
